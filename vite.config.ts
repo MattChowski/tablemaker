af12c352f95e3e33d7116a14e0fd7b46c0c23a42
Mateusz Wojciechowski <mateusz.wojciechowski@angrynerds.co>
2023-08-17 09:16:21 +0200

    fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'tablemaker',
+  base: '/tablemaker/',
   plugins: [react(), eslint()],
   resolve: {
-    alias: [{
-      find: '@', replacement: '/src'
-    }]
+    alias: [
+      {
+        find: '@',
+        replacement: '/src'
+      }
+    ]
   }
-})
+});
